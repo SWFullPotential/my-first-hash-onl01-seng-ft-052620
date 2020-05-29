f205d3900db560a6d7fa643249e250e3f29ca190
Samantha Wabi <swfullpotential@gmail.com>
2020-05-29 18:30:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,10 +17,8 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
 end
-
+the_manifest
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5, 
@@ -48,5 +46,5 @@ shipping_manifest["gun powder"] = "4"
 
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
